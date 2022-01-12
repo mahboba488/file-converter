5f1d919e8cf5da549e2c89c526e2bc47ec493d73
mahboba <misticse488@gmail.com>
2022-01-12 11:10:02 +0600

    header update

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".toggle").on("click" , function(){
+        if($(".item").hasClass("active")){
+            $(".item").removeClass("active")
+
+        }
+        else{
+
+            $(".item").addClass("active")
+
+        }
+    })
+})
+
+
